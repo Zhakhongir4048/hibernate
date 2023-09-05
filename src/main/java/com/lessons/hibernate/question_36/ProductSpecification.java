993aea8266b80c310203d80ccd85166624ab229e
Эсонов Жахонгир <dzhoha4048@mal.ru>
2023-09-05 21:49:50 +0300

    question 36 complete

--- src/main/java/com/lessons/hibernate/question_36/ProductSpecification.java
+++ src/main/java/com/lessons/hibernate/question_36/ProductSpecification.java
@@ -5,15 +5,15 @@ import org.springframework.data.jpa.domain.Specification;
 public class ProductSpecification {
 
     public static Specification<Product> equalsName(String name) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("name"), name);
+        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get(Product_.name), name);
     }
 
     public static Specification<Product> salaryGreaterThan(Integer salary) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.greaterThan(root.get("salary"), salary);
+        return (root, query, criteriaBuilder) -> criteriaBuilder.greaterThan(root.get(Product_.salary), salary);
     }
 
     public static Specification<Product> salaryLessThan(Integer salary) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.lessThan(root.get("salary"), salary);
+        return (root, query, criteriaBuilder) -> criteriaBuilder.lessThan(root.get(Product_.salary), salary);
     }
 
 }
---
+++ src/main/java/com/lessons/hibernate/question_36/Product_.java
@@ -0,0 +1,22 @@
+package com.lessons.hibernate.question_36;
+
+import jakarta.persistence.metamodel.SingularAttribute;
+import jakarta.persistence.metamodel.StaticMetamodel;
+
+import javax.annotation.processing.Generated;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Product.class)
+public class Product_ {
+
+    public static volatile SingularAttribute<Product, Integer> id;
+    public static volatile SingularAttribute<Product, String> name;
+    public static volatile SingularAttribute<Product, Integer> salary;
+    public static volatile SingularAttribute<Product, Short> age;
+
+    public static final String ID = "id";
+    public static final String NAME = "name";
+    public static final String SALARY = "salary";
+    public static final String AGE = "age";
+
+}
